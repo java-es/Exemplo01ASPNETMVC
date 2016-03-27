8a60572cf88eab9305fb7c66bb72fd29e770e8c2
Wanderson Alves Rodrigues <wandersonallves@yahoo.com.br>
2016-03-27 13:17:07 -0300

    Criação do exemplo Olá Mundo ASP.NET MVC.

---
+++ OlaMundo/OlaMundo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OlaMundo.Startup))]
+namespace OlaMundo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
